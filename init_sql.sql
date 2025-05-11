4fab1140d1ecb7a9a8883e1366cbdf7a40cf47bb
Evgeniy Prochan <72522187+prochanev-thw@users.noreply.github.com>
2025-05-11 14:02:06 +0300

    add init_sql.sql

---
+++ init_sql.sql
@@ -0,0 +1,16 @@
+CREATE TABLE example_table (
+    name VARCHAR(100),
+    age INTEGER,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
+INSERT INTO example_table (name, age) VALUES
+('Петр Петров', 25),
+('Анна Сидорова', 28),
+('Мария Кузнецова', 35);
+
+
+
+select * from example_table
