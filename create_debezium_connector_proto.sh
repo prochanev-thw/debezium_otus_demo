f50af05c19a39294dce1fae8aa837fd89c8b979a
Evgeniy Prochan <72522187+prochanev-thw@users.noreply.github.com>
2025-05-12 07:02:32 +0300

    add connector with proto

---
+++ create_debezium_connector_proto.sh
@@ -0,0 +1,23 @@
+curl -X POST http://localhost:8083/connectors \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "debezium-postgres-connector-proto",
+    "config": {
+      "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+      "database.hostname": "postgresql",
+      "database.port": "5432",
+      "database.user": "user",
+      "database.password": "user",
+      "database.dbname": "postgres",
+      "database.server.name": "postgres",
+      "table.include.list": "public.*",
+      "plugin.name": "pgoutput",
+      "slot.name": "debezium_slot_proto",
+      "topic.prefix": "dbzm__",
+      "publication.name": "debezium_pub",
+      "snapshot.mode": "never",
+
+      "value.converter": "io.confluent.connect.protobuf.ProtobufConverter",
+      "value.converter.schema.registry.url": "http://localhost:8081",
+    }
+  }'
